51ae44269148c64e2baa5d8b10256044b03f717c
NicolaeMunt <114097090+NicolaeMunt@users.noreply.github.com>
2022-11-02 20:24:00 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,60 @@
+const text= 1;
+let text_1=true;
+// text="Salut";
+text_1="salut";
+let score= null;
+let a= 5;
+let plus='+';
+let b= 2;
+score=a**b
+let nume_prenume=null;
+let jojo="ahalaimahalai";
+let nume="Nicolae";
+let prenume="Munteanu";
+nume_prenume=nume+" "+prenume
+console.log(nume_prenume);
+
+console.log(typeof jojo);
+console.log(score);
+console.log(typeof text);
+console.log(text_1);
+// alert("jopajirnaia")
+// prompt("tu esti debil?")
+let Jora=17;
+if(Jora>18) {
+    console.log("Jora este adult");
+}
+else if(Jora==18) {
+    console.log("Jora tot este adult");
+}
+else (Jora<18); {
+    console.log("Jora bate capu despre virsta");
+}
+
+let fruit ="child labour";
+switch(fruit) {
+    case "orange":
+        console.log("Oranges are 1.3$ per kg");
+        break;
+    case "Banana":
+        console.log("Bananas are 2$ per kg" );
+        break;
+    case "GRapefruit" :
+        console.log("Grapefruits are 4$ per kg"); 
+        break;
+    case "carrot":
+        console.log("WE DONT SELL CARROT") ;
+        break;
+    case "child labour":
+        console.log("OI OI OI THATS A SHAME...IF YOU PAY...");
+        break;
+    default:
+        console.log('SORRY,GO FUK YOSEF');
+
+}
+let i=7;
+while (i%2==0&& i<10);{
+    console.log("numarul este par");
+    i+=1;
+}
+
